cc8b56dd514ea48ef330f9e384c2611b51e37190
SwetaMakuwala <sweta.makuwala@gmail.com>
2022-10-16 20:16:05 +0530

    initial commit

---
+++ notification and observers/controller/SelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectionViewController.swift
+//  notification and observers
+//
+//  Created by sweta makuwala on 26/01/22.
+//
+
+import UIKit
+
+class SelectionViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ notification and observers/controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  notification and observers
+//
+//  Created by sweta makuwala on 26/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var imageDisplay: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
+}
+
